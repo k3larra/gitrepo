4f64af7289c5d9a9f56308dcde29abd243859521
Lars Holmberg <lars.holmberg@mah.se>
2023-01-20 18:21:52 +0100

    yes

--- API_1/script.js
+++ API_1/script.js
@@ -1,2 +1,7 @@
 const myHeading = document.querySelector("h1");
 myHeading.textContent = "API lab 1.1";
+// const texten = fetch("https://graph.facebook.com/v5.0/janedoe42?fields=id,name,email,picture");
+
+fetch('https://graph.facebook.com/v5.0/janedoe42?fields=id,name,email,picture', {credentials: 'include'})
+  .then((response) => response.json())
+  .then((data) => console.log(data));
