5b6adf157a464ef319c664f6ac402991f72181b3
Lars Holmberg <lars.holmberg@mah.se>
2023-01-21 11:24:04 +0100

    Update

--- API_1/script.js
+++ API_1/script.js
@@ -2,6 +2,40 @@ const myHeading = document.querySelector("h1");
 myHeading.textContent = "API lab 1.1";
 // const texten = fetch("https://graph.facebook.com/v5.0/janedoe42?fields=id,name,email,picture");
 
-fetch('https://graph.facebook.com/v5.0/janedoe42?fields=id,name,email,picture', {credentials: 'include'})
+/* const animals = fetch('animals.json', {credentials: 'include'})
   .then((response) => response.json())
   .then((data) => console.log(data));
+const jane = fetch('jane.json', {credentials: 'include'})
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data)
+    showJane(data)
+}); 
+
+function showJane(data){
+    console.log("this",data.getElementById)
+    var text_1 = document.getElementById("maintext");
+    text_1.textContent = data.id;
+} */
+
+
+/* fetch('https://cat-fact.herokuapp.com/facts', {credentials: 'include'})
+  .then((response) => response.json())
+  .then((data) => console.log(data)); */
+
+//const url = "https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=ec0e080aa149103de03727efeeeda642";
+const url = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=ec0e080aa149103de03727efeeeda642"
+fetch(url)
+.then((response) => {
+    return response.json();
+}).then((data) => {
+    showWeather(data)
+});
+
+function showWeather(data){
+    console.log(data)
+    console.log(data.main.temp)
+    console.log(data.weather[0].description)
+    var text_1 = document.getElementById("maintext");
+    text_1.textContent = "Town: "+data.name+" Weather: "+data.weather[0].description;
+}
